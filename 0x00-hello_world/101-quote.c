87e21a134de359dc1cf5f3f4ad3b7bd8626e6f3f
mukonyole <emukonyole@gmail.com>
2023-03-16 12:25:39 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdi0.h>
+#include <unistd.h>
+/**
+ * main - A c programme that prints a line to the standard error
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+	return (1);
+
+
+
+
+
+
+
+
+}
