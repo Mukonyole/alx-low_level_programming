25288c8f41e7f0901b81ba77548a1e9ad704dba8
mukonyole <emukonyole@gmail.com>
2023-03-17 12:59:47 -0700

    Combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * Return: Always 0
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
-	int j;
+	int digit1, digit2;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (j = 1 j < 10 ; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (i < j && i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i + j != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				
-				}
-			
-			
-			}
-		
-		
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-	
-	
 	}
+
 	putchar('\n');
+
 	return (0);
-}	
+}
