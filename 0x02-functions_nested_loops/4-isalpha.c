26d6c9a665744370a5dc3ba808a2e32e0ee10a01
mukonyole <emukonyole@gmail.com>
2023-03-21 11:08:22 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,14 +5,14 @@
  *
  * @c: parameter to be checked
  *
- * Return:1 if it is an alphabet
- * and otherwise
+ * Return: 1 if it is an alphabet
+ * and 0 otherwise
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
