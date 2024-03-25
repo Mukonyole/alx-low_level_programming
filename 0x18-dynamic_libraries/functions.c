d4718d473f8a7abc918bb0cc61deb69133f16824
Evans Mukonyole <emukonyole@gmail.com>
2024-03-25 01:31:10 -0700

    dynamic library with c functions

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/**
+ * add - it adds two integers.
+ * @a:first integer
+ * @b:second integer.
+ * Return: sum of two integers.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
+/**
+ * sub - it subtracts two integers.
+ * @a:first integer
+ * @b:second integer.
+ * Return: difference of two integers.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+
+}
+
+/**
+ * mul - it multiplies two integers.
+ * @a:first integer
+ * @b:second integer.
+ * Return: product of two integers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
+
+/**
+ * mod - mod of two integers.
+ * @a:first integer
+ * @b:second integer.
+ * Return: mod of two integers.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+
+}
+
+/**
+ * div - it divides two integers.
+ * @a:first integer
+ * @b:second integer.
+ * Return: division results of two integers.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
